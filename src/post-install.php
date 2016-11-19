4fdc8de77c127a9662c5c474064f7e6e56d8a364
Ulrich Schmidt <ulrich1992@web.de>
2016-11-19 12:21:03 +0100

    Datenbankschema wird angelegt

---
+++ src/post-install.php
@@ -0,0 +1,10 @@
+<?php
+Database::query("CREATE TABLE `{prefix}slider` ( `id` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(255) NOT NULL , `enabled` TINYINT(1) NOT NULL DEFAULT '1' , PRIMARY KEY (`id`)) ENGINE = InnoDB", true);
+
+Database::query("CREATE TABLE `{prefix}slider_pictures` ( `id` INT NOT NULL AUTO_INCREMENT , `file` VARCHAR(255) NOT NULL , `enabled` TINYINT NOT NULL DEFAULT '1' , `slider_id` INT NOT NULL , `position` INT NOT NULL DEFAULT '10' , PRIMARY KEY (`id`)) ENGINE = InnoDB;", true);
+
+Database::query("ALTER TABLE `{prefix}slider_pictures`
+  ADD CONSTRAINT fk_slider
+  FOREIGN KEY (slider_id) 
+  REFERENCES `{prefix}slider`(id)
+  ON DELETE CASCADE;", true);
